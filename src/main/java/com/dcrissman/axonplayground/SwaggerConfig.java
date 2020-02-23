bc406fe0fdcc12303fe561d689c55cc5c6ac2f47
Dennis Crissman <dcrissma@redhat.com>
2020-02-23 14:55:47 -0500

    fix swagger

--- src/main/java/com/dcrissman/axonplayground/SwaggerConfig.java
+++ src/main/java/com/dcrissman/axonplayground/SwaggerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,7 +20,7 @@ public class SwaggerConfig {
     public Docket apiDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.progressivecoder.es"))
+                .apis(RequestHandlerSelectors.basePackage("com.dcrissman"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
@@ -33,10 +32,10 @@ public class SwaggerConfig {
                 "App to demonstrate Event Sourcing using Axon and Spring Boot",
                 "1.0.0",
                 "Terms of Service",
-                new Contact("Saurabh Dashora", "progressivecoder.com", "coder.progressive@gmail.com"),
+                null,
                 "",
                 "",
                 Collections.emptyList());
     }
-    
+
 }
